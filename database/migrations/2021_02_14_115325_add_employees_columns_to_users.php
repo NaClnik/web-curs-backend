02b6f0396f32afa9f61d1769b23543a85ce15a07
Daniil Dzevelyuk <ddzevelyuk@mail.ru>
2021-02-14 15:03:40 +0300

    change db

---
+++ database/migrations/2021_02_14_115325_add_employees_columns_to_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddEmployeesColumnsToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('surname');
+            $table->string('name');
+            $table->string('patronymic');
+            $table->string('passport');
+            $table->double('salary');
+            $table->string('photo_path');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
